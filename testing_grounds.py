395e4f61cce90ae1608efa88abbeb2a733d577d6
Nick Jarvis <najarvis2016@gmail.com>
2018-04-29 23:00:48 -0400

    Removes prints, debugs, and formats the output.

--- testing_grounds.py
+++ testing_grounds.py
@@ -18,17 +18,6 @@ def can_fit(object_dim, volume):
         volume.remove(max(volume))
     return True
 
-def assert_valid(dim):
-    """Checks if the dimensions of an object / volume are valid."""
-    assert len(dim) == 3
-    try:
-        assert dim[0] > 0
-        assert dim[1] > 0
-        assert dim[2] > 0
-    except AssertionError as err:
-        print(dim)
-        raise err
-
 def get_dim(obj):
     """Returns the dimensions of a dimension-pos list"""
     return obj[:3]
@@ -65,32 +54,6 @@ def str_to_dim(string):
 
     return list(map(int, string.split(" "))) + [0, 0, 0]
 
-def debug_print(objects, volumes, finished_objects):
-    """Debug print. Pretty self explainitory. Remove in final version."""
-
-    # TODO: Remove
-    print("objects:")
-    if objects:
-        for obj in objects:
-            print("\tid={0}, l,w,h: {1},{2},{3} x,y,z:({4},{5},{6})".format(obj[0], *obj[1]))
-    else:
-        print("\t[]")
-    print("Volumes:")
-    if volumes:
-        for vol in volumes:
-            print("\tl,w,h: {0},{1},{2} x,y,z:({3},{4},{5})".format(*vol))
-    else:
-        print("\t[]")
-    print("Finished Objects:")
-    if finished_objects:
-        for obj in finished_objects:
-            print("\tid={0}, l,w,h: {1},{2},{3} x,y,z:({4},{5},{6})".format(obj[0], *obj[1]))
-    else:
-        print("\t[]")
-    print()
-    print("*" * 50)
-    print()
-
 def run(volumes, objects):
     """Does the main algorithm, and returns a list containing the dimensions
     and positions for each object."""
@@ -99,7 +62,6 @@ def run(volumes, objects):
 
     # Sort all the objects based on the largest of their dimensions
     objects = sorted(objects, key=lambda x: sorted(get_dim(x[1]), reverse=True), reverse=True)
-    debug_print(objects, volumes, finished_objects)
     while objects and volumes:
         current = objects.pop(0)
 
@@ -117,14 +79,10 @@ def run(volumes, objects):
         set_pos(current[1], get_pos(best_fitting))
         #num_sharing = len([dim for dim in current[1] if dim in best_fitting[:3]])
         num_sharing = num_sharing_dim(get_dim(current[1]), get_dim(best_fitting))
-        print(current[1])
 
         if num_sharing == 0:
-            print(0)
             # Shares no dimensions with the "best fit"
             # Create 3 new volumes
-            # print(volumes[0])
-            # print(current)
             finished_objects.append(current)
 
             # Volume on top of the object added
@@ -138,27 +96,11 @@ def run(volumes, objects):
 
             volumes.remove(best_fitting)
 
-            # TODO: Remove in final version.
-            try:
-                assert_valid(get_dim(top_volume))
-                assert_valid(get_dim(width_volume))
-                assert_valid(get_dim(length_volume))
-            except AssertionError as err:
-                debug_print(objects, volumes, finished_objects)
-                print("current: {}".format(current))
-                print("best_fitting: {}".format(best_fitting))
-                print("top_volume: {}".format(top_volume))
-                print("width_volume: {}".format(width_volume))
-                print("length_volume: {}".format(length_volume))
-                print(volumes_by_fit)
-                print(vars())
-                raise err
             volumes.append(top_volume)
             volumes.append(width_volume)
             volumes.append(length_volume)
 
         elif num_sharing == 1:
-            print(1)
             # 2 possible orientations, not equivalent
             # Shares one dimension with the best fit
             # Create 2 new volumes
@@ -180,8 +122,6 @@ def run(volumes, objects):
                 top_volume = current[1][:2] + [best_fitting[2] - current[1][2]] + best_fitting[3:5] + [current[1][2]]
                 side_volume = [best_fitting[0] - current[1][0]] + best_fitting[1:2] + [best_fitting[3] + current[1][0]] + best_fitting[3:6]
             if swap_best == 2:
-                print(current[1], best_fitting)
-                print([best_fitting[0]] + [best_fitting[1] - current[1][1]] + best_fitting[2:4] + [best_fitting[4] + current[1][1]] + [best_fitting[5]])
                 top_volume = [best_fitting[0] - current[1][0]] + current[1][1:3] + [best_fitting[3] + current[1][0]] + best_fitting[4:6]
                 side_volume = [best_fitting[0]] + [best_fitting[1] - current[1][1]] + best_fitting[2:4] + [best_fitting[4] + current[1][1]] + [best_fitting[5]]
 
@@ -190,7 +130,6 @@ def run(volumes, objects):
             volumes.append(side_volume)
 
         elif num_sharing == 2:
-            print(2)
             # Shares two dimensions with the best fit
             # Create 1 new volume
 
@@ -225,7 +164,6 @@ def run(volumes, objects):
             volumes.append(created_volume)
 
         elif num_sharing == 3:
-            print(3)
             # Volume is a perfect fit for the object
             # Create no new volumes
             set_dim(current[1], get_dim(best_fitting))
@@ -234,12 +172,7 @@ def run(volumes, objects):
             volumes.remove(best_fitting)
             finished_objects.append(current)
 
-        debug_print(objects, volumes, finished_objects)
-
-    print("ALGORITHM COMPLETE!!!!!\n")
-    debug_print(objects, volumes, finished_objects)
-
-    return finished_objects
+    return finished_objects, volumes
 
 if __name__ == "__main__":
     VOLUMES = []
@@ -261,4 +194,7 @@ if __name__ == "__main__":
             i += 1
             OBJ_STR = f.readline()
 
-    run(VOLUMES, OBJECTS)
+    final = run(VOLUMES, OBJECTS)
+    print("ID\tLocation\tOrientation")
+    for obj in final[0]:
+        print("{}\t{}\t{}".format(obj[0], get_pos(obj[1]), get_dim(obj[1])))
